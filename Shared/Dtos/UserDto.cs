aacb91e9b62785850dd21f73af4e9ce02980eb48
kaldren <drenski666@gmail.com>
2020-06-12 16:21:27 +0300

    Added id

--- Shared/Dtos/UserDto.cs
+++ Shared/Dtos/UserDto.cs
@@ -6,6 +6,7 @@ namespace Onebrb.Shared.Dtos
 {
     public class UserDto
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
