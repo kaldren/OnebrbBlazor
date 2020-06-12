4c82bc37a2b47a33ba416d423b70bddbe98a0752
kaldren <drenski666@gmail.com>
2020-06-12 14:12:28 +0300

    Added basic repository interface

---
+++ Server/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Onebrb.Server.Interfaces
+{
+    /// <summary>
+    /// Basic repository interface
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    public interface IRepository<TModel> where TModel : class, IEntity
+    {
+        Task Get(int id);
+    }
+}
